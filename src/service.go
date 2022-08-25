6144cf7cf1f1097e71b6e68b8ee783e654dc9fd8
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2022-08-25 16:42:40 +0200

    Added a graceful shutdown to the http server

--- src/service.go
+++ src/service.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	context2 "context"
+	"fmt"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
 	"microservice/handlers"
 	"microservice/vars"
 )
@@ -51,4 +56,39 @@ func main() {
 			http.HandlerFunc(handlers.CreateNewConsumer),
 		),
 	).Methods("PUT")
+
+	// Configure the HTTP server
+	server := &http.Server{
+		Addr:         fmt.Sprintf("0.0.0.0:%s", vars.HttpListenPort),
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      router,
+	}
+
+	// Start the server and log errors that happen while running it
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.WithError(err).Fatal("An error occurred while starting the http server")
+		}
+	}()
+
+	// Set up the signal handling to allow the server to shut down gracefully
+
+	cancelSignal := make(chan os.Signal, 1)
+	signal.Notify(cancelSignal, os.Interrupt)
+
+	// Block further code execution until the shutdown signal was received
+	<-cancelSignal
+
+	context, cancel := context2.WithTimeout(context2.Background(), time.Second*15)
+	defer cancel()
+
+	go func() {
+		err := server.Shutdown(context)
+		if err != nil {
+			log.WithError(err).Fatal("An error occurred while stopping the http server")
+		}
+	}()
+	log.Info("Shutting down the microservice...")
 }
