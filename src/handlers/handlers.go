b23a0470eb5be70043220e346813e4c491e0201b
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2022-08-25 13:38:21 +0200

    Added code for running GET operations on the microservice

--- src/handlers/handlers.go
+++ src/handlers/handlers.go
@@ -1,13 +1,19 @@
 package handlers
 
 import (
+	"database/sql"
 	"encoding/json"
 	"net/http"
+	"regexp"
+	"strconv"
 	"strings"
 
+	"github.com/lib/pq"
+	geojson "github.com/paulmach/go.geojson"
 	log "github.com/sirupsen/logrus"
 	"microservice/errors"
 	"microservice/helpers"
+	"microservice/structs"
 	"microservice/vars"
 )
 
@@ -120,7 +126,142 @@ func validateRequestParameters(w http.ResponseWriter, r *http.Request) {
 }
 
 func returnConsumerInformation(w http.ResponseWriter, r *http.Request) {
-	// TODO: Implement code logic
+	logger := log.WithFields(log.Fields{
+		"middleware": false,
+		"title":      "ReturnConsumerInformation",
+	})
+	// Validate the parameters for the get request if they have been set
+	validateRequestParameters(w, r)
+	// Create an empty database query string in preparation for the queries
+	var databaseQuery string
+	// Check which parameters are available for the database query
+	usageAboveAvailable := r.URL.Query().Has("usage_above")
+	consumerIdsAvailable := r.URL.Query().Has("id")
+	areaKeysAvailable := r.URL.Query().Has("in")
+	// Prepare objects for the query results
+	var rows *sql.Rows
+	var queryError error
+	switch {
+	case usageAboveAvailable && consumerIdsAvailable && areaKeysAvailable:
+		databaseQuery = `SELECT id, name, ST_ASGeoJSON(location) FROM water_usage.consumers
+						 WHERE id IN (SELECT consumer FROM water_usage.usages WHERE value > $1) AND
+						       id = any($2) AND
+						       ST_CONTAINS((SELECT geom FROM geodata.shapes WHERE key = any($3)), location)`
+		// Get the needed parameters from the query
+		usageAbove, _ := strconv.Atoi(r.URL.Query().Get("usage_above"))
+		consumerIds := r.URL.Query()["id"]
+		areaKeys := r.URL.Query()["in"]
+		logger.Info("Executing a query in the database")
+		rows, queryError = vars.PostgresConnection.Query(
+			databaseQuery, usageAbove, pq.Array(consumerIds), pq.Array(areaKeys),
+		)
+		break
+	case usageAboveAvailable && consumerIdsAvailable && !areaKeysAvailable:
+		databaseQuery = `SELECT id, name, ST_ASGeoJSON(location) FROM water_usage.consumers
+						 WHERE id IN (SELECT consumer FROM water_usage.usages WHERE value > $1) AND
+						       id = any($2)`
+		// Get the needed parameters from the query
+		usageAbove, _ := strconv.Atoi(r.URL.Query().Get("usage_above"))
+		consumerIds := r.URL.Query()["id"]
+		logger.Info("Executing a query in the database")
+		rows, queryError = vars.PostgresConnection.Query(databaseQuery, usageAbove, pq.Array(consumerIds))
+		break
+	case usageAboveAvailable && !consumerIdsAvailable && areaKeysAvailable:
+		databaseQuery = `SELECT id, name, ST_ASGeoJSON(location) FROM water_usage.consumers
+						 WHERE id IN (SELECT consumer FROM water_usage.usages WHERE value > $1) AND
+						       ST_CONTAINS((SELECT geom FROM geodata.shapes WHERE key = any($2)), location)`
+		// Get the needed parameters from the query
+		usageAbove, _ := strconv.Atoi(r.URL.Query().Get("usage_above"))
+		areaKeys := r.URL.Query()["in"]
+		logger.Info("Executing a query in the database")
+		rows, queryError = vars.PostgresConnection.Query(databaseQuery, usageAbove, pq.Array(areaKeys))
+		break
+	case !usageAboveAvailable && consumerIdsAvailable && areaKeysAvailable:
+		databaseQuery = `SELECT id, name, ST_ASGeoJSON(location) FROM water_usage.consumers
+						 WHERE id = any($1) AND
+						       ST_CONTAINS((SELECT geom FROM geodata.shapes WHERE key = any($2)), location)`
+		// Get the needed parameters from the query
+		consumerIds := r.URL.Query()["id"]
+		areaKeys := r.URL.Query()["in"]
+		logger.Info("Executing a query in the database")
+		rows, queryError = vars.PostgresConnection.Query(databaseQuery, pq.Array(consumerIds), pq.Array(areaKeys))
+		break
+	case usageAboveAvailable && !consumerIdsAvailable && !areaKeysAvailable:
+		databaseQuery = `SELECT id, name, ST_ASGeoJSON(location) FROM water_usage.consumers
+						 WHERE id IN (SELECT consumer FROM water_usage.usages WHERE value > $1)`
+		// Get the needed parameters from the query
+		usageAbove, _ := strconv.Atoi(r.URL.Query().Get("usage_above"))
+		logger.Info("Executing a query in the database")
+		rows, queryError = vars.PostgresConnection.Query(databaseQuery, usageAbove)
+		break
+	case !usageAboveAvailable && consumerIdsAvailable && !areaKeysAvailable:
+		databaseQuery = `SELECT id, name, ST_ASGeoJSON(location) FROM water_usage.consumers
+						 WHERE id = any($1)`
+		// Get the needed parameters from the query
+		consumerIds := r.URL.Query()["id"]
+		logger.Info("Executing a query in the database")
+		rows, queryError = vars.PostgresConnection.Query(databaseQuery, pq.Array(consumerIds))
+		break
+	case !usageAboveAvailable && !consumerIdsAvailable && areaKeysAvailable:
+		databaseQuery = `SELECT id, name, ST_ASGeoJSON(location) FROM water_usage.consumers
+						 WHERE ST_CONTAINS((SELECT geom FROM geodata.shapes WHERE key = any($1)), location)`
+		// Get the needed parameters from the query
+		areaKeys := r.URL.Query()["in"]
+		logger.Info("Executing a query in the database")
+		rows, queryError = vars.PostgresConnection.Query(databaseQuery, pq.Array(areaKeys))
+		break
+	default:
+		databaseQuery = `SELECT id, name, ST_ASGeoJSON(location) FROM water_usage.consumers`
+		logger.Info("Executing a query in the database")
+		rows, queryError = vars.PostgresConnection.Query(databaseQuery)
+		break
+	}
+	if queryError != nil {
+		logger.WithError(queryError).Error("An error occurred during the database query")
+		helpers.SendRequestError(errors.DatabaseQueryError, w)
+		return
+	}
+	var consumers []structs.Consumer
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+			logger.WithError(err).Error("Unable to close the rows from the database")
+			helpers.SendRequestError(errors.DatabaseQueryError, w)
+			return
+		}
+	}(rows)
+	// Iterate through the rows and construct the consumer objects
+	for rows.Next() {
+		var uuid string
+		var name string
+		var location geojson.Geometry
+
+		scanError := rows.Scan(&uuid, &name, &location)
+		if scanError != nil {
+			logger.WithError(scanError).Error("An error occurred while iterating through the result rows of the query.")
+			helpers.SendRequestError(errors.DatabaseQueryError, w)
+			return
+		}
+
+		consumers = append(consumers, structs.Consumer{
+			UUID:     uuid,
+			Name:     name,
+			Location: location,
+		})
+	}
+	// Now check the length of the consumers array
+	if len(consumers) == 0 {
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	// Since the query has found a consumer set the header to json and return the response
+	w.Header().Set("Content-Type", "application/json")
+	encodingError := json.NewEncoder(w).Encode(consumers)
+	if encodingError != nil {
+		logger.WithError(encodingError).Error("An error occurred while returning the response")
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
 }
 
 func createNewConsumer(w http.ResponseWriter, r *http.Request) {
