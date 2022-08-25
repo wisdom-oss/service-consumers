d8b85bfc529edccf3c8dbef2ad11b67b9f6b705b
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2022-08-25 13:16:12 +0200

    Added error for having invalid query parameters

--- src/errors/errors.go
+++ src/errors/errors.go
@@ -14,11 +14,13 @@ import (
 const UnauthorizedRequest = "UNAUTHORIZED_REQUEST"
 const MissingScope = "SCOPE_MISSING"
 const UnsupportedHTTPMethod = "UNSUPPORTED_METHOD"
+const InvalidQueryParameter = "INVALID_QUERY_PARAMETER"
 
 var errorTitle = map[string]string{
 	UnauthorizedRequest:   "Unauthorized Request",
 	MissingScope:          "Forbidden",
 	UnsupportedHTTPMethod: "Unsupported HTTP Method",
+	InvalidQueryParameter: "Invalid Query Parameter",
 }
 
 var errorDescription = map[string]string{
@@ -27,12 +29,15 @@ var errorDescription = map[string]string{
 		"Your authorization information did not contain the required scope.",
 	UnsupportedHTTPMethod: "The used HTTP method is not supported by this microservice. " +
 		"Please check the documentation for further information",
+	InvalidQueryParameter: "One or more parameters set for the request are not in a valid format. " +
+		"Please check your request and read the API documentation.",
 }
 
 var httpStatus = map[string]int{
 	UnauthorizedRequest:   http.StatusUnauthorized,
 	MissingScope:          http.StatusForbidden,
 	UnsupportedHTTPMethod: http.StatusMethodNotAllowed,
+	InvalidQueryParameter: http.StatusBadRequest,
 }
 
 func NewRequestError(errorCode string) structs.RequestError {
