e5d420f968a44842f3a17692081af5efd5840c7d
Jan Eike Suchard <jan@suchard.cloud>
2023-11-20 23:01:35 +0100

    adding base consumer query and filter queries

--- resources/queries.sql
+++ resources/queries.sql
@@ -1,5 +1,20 @@
--- name: template-query
-CREATE TABLE template (
-    id serial,
-    name text
-);
+-- name: get-consumers
+SELECT
+    id,
+    name,
+    description,
+    address,
+    ST_AsGeoJSON(location) as location,
+    usage_type,
+    additional_properties
+FROM
+    consumers.consumers;
+
+-- name: filter-ids
+id = any($1);
+
+-- name: filter-usage-amount
+id IN (SELECT consumer FROM water_usages.usages WHRERE usages.amount > $1);
+
+-- name: filter-location
+ST_CONTAINS(ST_UNION(ARRAY((SELECT geom FROM geodata.shapes WHERE key = any($2)))), location);
