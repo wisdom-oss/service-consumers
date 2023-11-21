4166482c1dc7b9337ae8ac33bbc308eb8a259c35
Jan Eike Suchard <jan@suchard.cloud>
2023-11-21 13:23:21 +0100

    fixing typo in query leading to 500 error

--- resources/queries.sql
+++ resources/queries.sql
@@ -14,7 +14,7 @@ FROM
 id = any($1);
 
 -- name: filter-usage-amount
-id IN (SELECT consumer FROM water_usages.usages WHRERE usages.amount > $1);
+id IN (SELECT consumer FROM water_usage.usages WHERE consumer IS NOT NULL AND usages.amount > $1);
 
 -- name: filter-location
 ST_CONTAINS(ST_UNION(ARRAY((SELECT geom FROM geodata.shapes WHERE key = any($1)))), location);
