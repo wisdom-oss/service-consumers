962ae7eebb8fd04535ff4f72a12ec955c21457fb
Jan Eike Suchard <jan@suchard.cloud>
2023-11-27 13:33:19 +0100

    adding update route

--- routes/update-consumer.go
+++ routes/update-consumer.go
@@ -4,8 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 
+	"github.com/blockloop/scan/v2"
 	"github.com/go-chi/chi/v5"
+	"github.com/lib/pq"
 	"github.com/rs/zerolog/log"
 	wisdomMiddleware "github.com/wisdom-oss/microservice-middlewares/v3"
 
@@ -21,14 +24,81 @@ func UpdateConsumer(w http.ResponseWriter, r *http.Request) {
 	// get the id of the consumer that shall be updated
 	consumerID := chi.URLParam(r, "consumer-id")
 
-	// now parse the request body into the new consumer
+	// now get the consumer that has the id
+	baseQuery, err := globals.SqlQueries.Raw("get-consumers")
+	if err != nil {
+		log.Error().Err(err).Msg("unable to build query")
+		errorHandler <- fmt.Errorf("unable to build query: %w", err)
+		<-statusChannel
+		return
+	}
+	idFilter, err := globals.SqlQueries.Raw("filter-ids")
+	if err != nil {
+		log.Error().Err(err).Msg("unable to build query")
+		errorHandler <- fmt.Errorf("unable to build query: %w", err)
+		<-statusChannel
+		return
+	}
+
+	// now merge the two filters
+	sql := fmt.Sprintf(`%s WHERE %s`, strings.Trim(baseQuery, ";"), idFilter)
+
+	// now prepare the query
+	query, err := globals.Db.Prepare(sql)
+	if err != nil {
+		log.Error().Err(err).Msg("unable to prepare database query")
+		errorHandler <- fmt.Errorf("unable to preparse database query: %w", err)
+		<-statusChannel
+		return
+	}
+
+	// now execute the sql query
+	rows, err := query.Query(pq.Array([]string{consumerID}))
+	if err != nil {
+		log.Error().Err(err).Msg("unable to query the database")
+		errorHandler <- fmt.Errorf("unable to query database: %w", err)
+		<-statusChannel
+		return
+	}
+
+	// now scan the query results into a single consumer
 	var consumer types.Consumer
-	err := json.NewDecoder(r.Body).Decode(&consumer)
+	err = scan.Row(&consumer, rows)
+	if err != nil {
+		log.Error().Err(err).Msg("unable to parse database query results")
+		errorHandler <- fmt.Errorf("unable to parse query result: %w", err)
+		<-statusChannel
+		return
+	}
+
+	// now parse the request body into the new consumer
+	var updatedConsumerRepresentation types.Consumer
+	err = json.NewDecoder(r.Body).Decode(&updatedConsumerRepresentation)
 	if err != nil {
 		log.Error().Err(err).Msg("unable to decode request body into consumer")
 		errorHandler <- fmt.Errorf("unable to decode request body into consumer: %w", err)
 		<-statusChannel
 		return
+	}
+
+	// now check which fields need to be updated
+	if consumer.Name != updatedConsumerRepresentation.Name {
+		consumer.Name = updatedConsumerRepresentation.Name
+	}
+	if consumer.Description != updatedConsumerRepresentation.Description {
+		consumer.Description = updatedConsumerRepresentation.Description
+	}
+	if consumer.Address != updatedConsumerRepresentation.Address {
+		consumer.Address = updatedConsumerRepresentation.Address
+	}
+	if consumer.Location != updatedConsumerRepresentation.Location {
+		consumer.Location = updatedConsumerRepresentation.Location
+	}
+	if consumer.UsageType != updatedConsumerRepresentation.UsageType {
+		consumer.UsageType = updatedConsumerRepresentation.UsageType
+	}
+	if consumer.AdditionalProperties != updatedConsumerRepresentation.AdditionalProperties {
+		consumer.AdditionalProperties = updatedConsumerRepresentation.AdditionalProperties
 	}
 
 	// now write the consumer into the database
